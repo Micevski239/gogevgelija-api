9d256ea9f18db9169d6ef2182440f8c452b430d4
Micevski239 <filipmicevski04@gmail.com>
2025-10-09 15:03:42 +0200

    temp switch2

--- api/settings.py
+++ api/settings.py
@@ -111,7 +111,14 @@ if not DEBUG and 'postgres' in db_url.lower():
     }
 
 # Auth
-REST_FRAMEWORK = {"DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.AllowAny"]}
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticatedOrReadOnly",
+    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ],
+}
 
 
 SIMPLE_JWT = {
